352090e9c1c0d8223cb5b6d8db5e2dfd6e5c8af1
tio nam <namtio.end0113@gmail.com>
2022-10-15 00:09:59 +0900

    test

---
+++ StagingTasks/php2/LaravelNewsPosted.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaravelNews</title>
+    <link rel="stylesheet" href="LaravelNews2.css">
+</head> <header>
+        <div class="header-logo">
+        <a href="LaravelNews1.html">Laravel News</a>
+        </div>
+    </header>
+<body>
+
+    
+</body>
+</html>
